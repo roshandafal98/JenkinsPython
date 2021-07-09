9be52284cf3b8cefae560e56ebef67c0302ad671
roshandafal98 <87060217+roshandafal98@users.noreply.github.com>
2021-07-09 12:32:03 +0530

    Update pythonfile.py

--- pythonfile.py
+++ pythonfile.py
@@ -1,1 +1,12 @@
+file=open("C:/Users/Roshan Dafal/Desktop/Python/sample.log ")
+x=file.readlines()
+count=0
+l=[]
+
+for i in x:
+  if 'rules violated' in l:
+  l.append(i)
+for i in l:
+  count=count+int(i[1:len(i)-15])
+print(count)  
 
